9b20ee105b82d75d6cb548e2a347184b61cb3dc8
Graciela Tabera <gra.tavi@gmail.com>
2021-03-28 17:21:56 -0300

    validacion general del admin y alert

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 function campoRequerido(clave) {
     if (clave.value.trim() === "") {
         clave.className = 'form-control is-invalid';
+        return false;
     } else {
         clave.className = 'form-control is-valid';
+        return true;
     }
 }
 
@@ -10,8 +12,26 @@ function validarMail(email) {
     let expresion = /\w+@\w+\.[a-z]{2,}$/;
     if (email.value.trim() != '' && expresion.test(email.value)) {
         email.className = 'form-control is-valid';
+        return true;
     } else {
         email.className = 'form-control is-invalid';
+        return false;
 
     }
+}
+
+function validarLogin(event) {
+    event.preventDefault();
+    if (validarMail(document.getElementById('email')) == true && campoRequerido(document.getElementById('clave'))) {
+        if (document.getElementById('email').value == "admin@rollflix.com" && document.getElementById('clave').value == "admin") {
+
+            window.location.href = "http://127.0.0.1:5500/netflix.github.io/admin.html"
+        } else {
+            alert("Usuario no valido")
+        }
+
+    } else {
+        alert("Campos Incorrectos")
+    }
+
 }
